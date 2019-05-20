043cba6f69386024acc4ae300e414f732a6dad05
Ken Ruf <ruf.kenichi@gmail.com>
2019-05-19 21:05:02 -0500

    added name, species, homeworld, birth_year, gender, eye_color, and hair_color as props for Character

--- starwars/src/components/character.js
+++ starwars/src/components/character.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './StarWars.css';
 
-function Character() {
+function Character(props) {
   return (
     <div>
+      {props.name}
+      {props.species}
+      {props.homeworld}
+      {props.birth_year}
+      {props.gender}
+      {props.eye_color}
+      {props.hair_color}
     </div>
   )
 }
