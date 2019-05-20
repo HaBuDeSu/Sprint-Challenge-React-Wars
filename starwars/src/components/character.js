898f7753d04c119ba8a23f6f2f221f0f7f5c8974
Ken Ruf <ruf.kenichi@gmail.com>
2019-05-19 20:41:51 -0500

    added characterList and character files

---
+++ starwars/src/components/character.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './StarWars.css';
+
+const character = () => {
+  <div>
+  </div>
+}
+
+export default character;
---
+++ starwars/src/components/characterList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './StarWars.css';
+
+const characterList = () => {
+  <div>
+  </div>
+}
+
+export default characterList;
