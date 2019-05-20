ce4bb4b8f5cc3c68225e24d2f484107623f2b188
Ken Ruf <ruf.kenichi@gmail.com>
2019-05-19 21:40:39 -0500

    did some basic styling. done with MVP moving on to stretch

--- starwars/src/components/character.js
+++ starwars/src/components/character.js
@@ -5,8 +5,8 @@ function Character(props) {
   return (
     <div className='character-box'>
       <p>Name: {props.name}</p>
-      // <p>Species: {props.species.name}</p>
-      // <p>Home World: {props.homeworld}</p>
+      {/*<p>Species: {props.species.name}</p>
+      <p>Home World: {props.homeworld}</p>*/}
       <p>Birth Year: {props.birth_year}</p>
       <p>Gender: {props.gender}</p>
       <p>Eye Color: {props.eye_color}</p>
